990c297f10f906d8fb0acc3406b50352bb5b6fe2
Danilo Paes <danilo.paes@pm.me>
2023-07-14 01:14:48 +0200

    refact + lint

--- windsor_cli/src/client_helpers/third_party_configs.py
+++ windsor_cli/src/client_helpers/third_party_configs.py
@@ -3,15 +3,15 @@ from enum import Enum
 
 class GoogleDrive(Enum):
     """
-        This module is responsible for hold the configurations
-        related to the download parameters of google drive.
+    This module is responsible for hold the configurations
+    related to the download parameters of google drive.
 
-        It could change any time.
+    It could change any time.
 
-        Attributes:
-            TEST_TASK_DATA_FILE: The Id of the file on google drive.
-            GOOGLE_DRIVE_DOWNLOAD_URL: The main endpoint.
-            GOOGLE_DRIVE_DOWNLOAD_PARAMS = Standard params for download.
+    Attributes:
+        TEST_TASK_DATA_FILE: The Id of the file on google drive.
+        GOOGLE_DRIVE_DOWNLOAD_URL: The main endpoint.
+        GOOGLE_DRIVE_DOWNLOAD_PARAMS = Standard params for download.
     """
 
     TEST_TASK_DATA_FILE = ""
